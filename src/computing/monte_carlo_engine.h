399d277ea19f9b78f0dac11daa0b692d4030ab0e
Finn Willems <willems@nuclear-training.de>
2026-03-13 10:09:20 +0100

    MC Performance Enhancements

--- src/computing/monte_carlo_engine.h
+++ src/computing/monte_carlo_engine.h
@@ -6,22 +6,31 @@
 #include <random>
 #include <chrono>
 #include <algorithm>
+#include <numeric>
+#include <thread>
 #include "../models/assets.h"
 #include "asset_computing.h"
 
 #define Mat Eigen::MatrixXd
 #define Vec Eigen::VectorXd
 
-thread_local std::mt19937 rng(std::chrono::system_clock::now().time_since_epoch().count());
+thread_local std::mt19937 rng = [](){
+    std::seed_seq seq {
+        static_cast<uint64_t>(std::chrono::high_resolution_clock::now().time_since_epoch().count()),
+        static_cast<uint64_t>(std::hash<std::thread::id>{}(std::this_thread::get_id())),
+        static_cast<uint64_t>(reinterpret_cast<uintptr_t>(&seq))//Stack adress for extra entropy
+    };
+    return std::mt19937(seq);
+}();
 
 namespace monte_carlo {
 
     struct sim_preset {
-        std::vector<double> s_0;
+        size_t n_assets;
         std::vector<double> sigma;
         std::vector<double> mu;
         std::vector<double> df;
-        std::vector<double> weight;
+        Vec weight;
 
         size_t n_sims;
         size_t horizon_days;
@@ -47,19 +56,17 @@ namespace monte_carlo {
         preset.horizon_days = horizon_days;
         preset.portfolio_value = portfolio_value;
 
-        preset.s_0.reserve(assets.size());
+        preset.n_assets = assets.size();
         preset.sigma.reserve(assets.size());
         preset.mu.reserve(assets.size());
         preset.df.reserve(assets.size());
 
-        preset.weight.reserve(assets.size());
+        preset.weight = Vec::Map(weights.data(), weights.size());
+
         for (size_t i = 0; i < assets.size(); i++) {
-            //preset.s_0.push_back(assets[i].data_points[assets[i].n_data_points - 1].adjclose);
-            preset.s_0.push_back(1);
             preset.sigma.push_back(asset_compute::volatility(assets[i]));
             preset.mu.push_back(asset_compute::avg_log_return(assets[i])); 
             preset.df.push_back(asset_compute::dof_excess_kurtosis(assets[i]));
-            preset.weight.push_back(weights[i]);
         }
         Mat cov_matrix = asset_compute::compute_covariance_matrix(assets);
         asset_compute::normalize_covariance_matrix(cov_matrix);
@@ -67,27 +74,47 @@ namespace monte_carlo {
         return preset;
     }
 
-    sim_result run_simulation(const sim_preset& preset){
-        const size_t n_assets = preset.s_0.size();
-        sim_result result;
-        result.final_portfolio_values.reserve(preset.n_sims);
-
-        for(size_t i = 0; i < preset.n_sims; ++i){
-            Vec S = Vec::Map(preset.s_0.data(), n_assets);
+    void simulation_job(const sim_preset& preset, int8_t id, int32_t load_size, double* res_out){
+        std::vector<std::student_t_distribution<double>> dists;
+        dists.reserve(preset.n_assets);
+        std::vector<double> drifts(preset.n_assets);
+        for (size_t k = 0; k < preset.n_assets; ++k) {
+            dists.emplace_back(preset.df[k]);
+            drifts[k] = preset.mu[k] - 0.5 * preset.sigma[k] * preset.sigma[k];
+        }
+        Vec S(preset.n_assets);
+        Vec Z(preset.n_assets);
+        Vec Eps(preset.n_assets);
+        for(size_t i = 0; i < load_size && id * load_size + i < preset.n_sims; ++i){
+            S.setOnes();
             for(size_t d = 0; d < preset.horizon_days; ++d){
-                Vec Z(n_assets);
-                for(size_t k = 0; k < n_assets; ++k){
-                    std::student_t_distribution<double> dist(preset.df[k]);
-                    Z(k) = dist(rng);
+                for(size_t k = 0; k < preset.n_assets; ++k){
+                    Z[k] = dists[k](rng);
                 }
-                auto Eps = preset.cholesky_cov_mat * Z;
-                for(size_t j = 0; j < n_assets; ++j){
-                    S(j) *= std::exp((preset.mu[j] - 0.5 * preset.sigma[j] * preset.sigma[j])+ preset.sigma[j] * Eps(j));
+                Eps.noalias() = preset.cholesky_cov_mat * Z;
+                for(size_t j = 0; j < preset.n_assets; ++j){
+                    S(j) *= std::exp(drifts[j] + preset.sigma[j] * Eps(j));
                 }
             }
-            result.final_portfolio_values.push_back((S.dot(Vec::Map(preset.weight.data(), preset.weight.size()))) * preset.portfolio_value);
+            res_out[id*load_size + i] = (S.dot(preset.weight)) * preset.portfolio_value;
         }
+    }
 
+    sim_result run_simulation(const sim_preset& preset){
+        sim_result result;
+        result.final_portfolio_values.resize(preset.n_sims);
+        unsigned int n_threads = std::thread::hardware_concurrency() > 0 ? std::thread::hardware_concurrency() : 4;
+        std::vector<std::thread> threads;
+        int32_t load_size = static_cast<int32_t>(preset.n_sims / n_threads);
+        for(unsigned int i = 0; i < n_threads; ++i){
+            threads.emplace_back(simulation_job, std::cref(preset), i, load_size, result.final_portfolio_values.data());
+        }
+        //work on remaining simulations in main thread if n_sims is not perfectly divisible by n_threads
+        simulation_job(std::cref(preset), n_threads, load_size, result.final_portfolio_values.data());
+
+        for(auto& t : threads){
+            t.join();
+        }
 
         std::sort(result.final_portfolio_values.begin(), result.final_portfolio_values.end());
 
